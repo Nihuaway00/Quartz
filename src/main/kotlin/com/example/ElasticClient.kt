d2730397e916b181cd0392ba35125d7e8973563a
Nihuaway00 <nikitaemaeny@gmail.com>
2024-10-03 15:08:47 +0300

    wrote elastic client

--- src/main/kotlin/com/example/ElasticClient.kt
+++ src/main/kotlin/com/example/ElasticClient.kt
@@ -1,6 +1,8 @@
 package com.example
 
+import io.ktor.server.application.*
 import co.elastic.clients.elasticsearch.ElasticsearchClient
+import co.elastic.clients.elasticsearch.indices.rollover.IndexRolloverMappingBuilders.single
 import co.elastic.clients.json.jackson.JacksonJsonpMapper
 import co.elastic.clients.transport.rest_client.RestClientTransport
 import io.ktor.utils.io.core.*
@@ -13,16 +15,20 @@ class ElasticClient : Closeable {
     val client: ElasticsearchClient
 
     init {
-        restClient = RestClient.builder(HttpHost("localhost", 9200)).build()
+        // Получаем переменные окружения или используем значения по умолчанию
+        val host = System.getenv("ELASTIC_HOST") ?: "localhost"
+        val port = System.getenv("ELASTIC_PORT")?.toInt() ?: 9200
+
+        restClient = RestClient.builder(HttpHost(host, port)).build()
         transport = RestClientTransport(restClient, JacksonJsonpMapper())
 
         client = ElasticsearchClient(transport)
     }
 
     override fun close() {
-        try{
+        try {
             restClient.close()
-        } catch (e: Exception){
+        } catch (e: Exception) {
             e.printStackTrace()
         }
     }
